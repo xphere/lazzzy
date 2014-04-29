4554cf74e16a6f4b200cec0e86c37a29662af291
Berny Cantos <be@rny.cc>
2014-04-29 21:59:01 +0200

    add phpunit tests

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ *  This file is part of the Berny\Lazzzy test suite
+ *
+ * (c) Berny Cantos <be@rny.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lazzzy\Tests;
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+}
